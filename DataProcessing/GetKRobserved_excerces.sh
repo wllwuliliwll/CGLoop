7ac667b96a1837478efe8b1113c6d5b2efffab96
wllwuliliwll <wllwulili@163.com>
2025-01-11 12:55:53 +0800

    Update GetKRobserved_excerces.sh

--- DataProcessing/GetKRobserved_excerces.sh
+++ DataProcessing/GetKRobserved_excerces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {20..22}
 do
-java -jar "/home/wulili/juicer_tools.jar" dump observed KR "/mnt/sda/wll/CGLoopData/HiC/Rwa_Cell/IMR90/GSE63525_IMR90_combined_30.hic" $i $i BP 5000 /mnt/sda/wll/CNNCNN-NewClear/IMR90Data/Interaction_frequency/chr$i-5kb.KRobserved
+java -jar "/path/of/juicer_tools.jar" dump observed KR "/path/of/file.hic" $i $i BP 5000 /path/of/Interaction_frequency/chr$i-5kb.KRobserved
 echo "chr"$i"已完成"
 done
