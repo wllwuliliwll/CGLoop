a54e93da7f1e3a806e882cc3e8153fdf6487f52e
wllwuliliwll <wllwulili@163.com>
2025-01-11 12:53:47 +0800

    Update GetBigMatrix_Cells_KRobserved.sh

--- DataProcessing/GetBigMatrix_Cells_KRobserved.sh
+++ DataProcessing/GetBigMatrix_Cells_KRobserved.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {20..22}
 do
-java -jar "/home/wulili/juicer_tools.jar" dump observed KR "/mnt/sda/wll/CGLoopData/HiC/Rwa_Cell/IMR90/GSE63525_IMR90_combined_30.hic" $i $i BP 5000 /mnt/sda/wll/CNNCNN-NewClear/IMR90Data/frequence_matrix/KR_matrix_5kb.chr${i}_tmp -d
-python remove_nan.py /mnt/sda/wll/CNNCNN-NewClear/IMR90Data/frequence_matrix/KR_matrix_5kb.chr${i}_tmp /mnt/sda/wll/CNNCNN-NewClear/IMR90Data/frequence_matrix/KR_matrix_5kb.chr$i
+java -jar "/path/of/juicer_tools.jar" dump observed KR "path/of/file.hic" $i $i BP 5000 /path/of/frequence_matrix/KR_matrix_5kb.chr${i}_tmp -d
+python remove_nan.py /path/of/frequence_matrix/KR_matrix_5kb.chr${i}_tmp /path/of/frequence_matrix/KR_matrix_5kb.chr$i
 echo "chr"$i"已完成"
 done
